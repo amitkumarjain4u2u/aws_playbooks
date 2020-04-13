d9f1962e7313f5ac82574214e4e8f4c9aa84a6a1
amitkumarjain4u2u <amitkumarjain4u2u@gmail.com>
2020-04-13 15:38:06 +0000

    LOADING AWS PLAYBOOKS

--- update.sh
+++ update.sh
@@ -1,1 +1,10 @@
-sasd
+git pull
+cp -r /home/ansible/pdauto/* /home/ansible/aws_playbooks/
+rm ec2_key_pair.ppk
+rm ec2_key_pair.ppm
+git status
+git add .
+git commit -m "LOADING AWS PLAYBOOKS"
+git push
+git status
+git pull
